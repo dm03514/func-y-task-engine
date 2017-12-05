c6d08631bd1cfa73d689b935027bfda0a7892798
daniel mican <dmican@daniels-MacBook-Pro.local>
2017-12-04 21:14:53 -0500

    removed json logger

--- funcytaskengine/settings.py
+++ funcytaskengine/settings.py
@@ -4,16 +4,6 @@ import sys
 DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 DEFAULT_LOG_LEVEL = 'DEBUG'
 
-'''
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter()
-ch.setFormatter(formatter)
-root.addHandler(ch)
-'''
 
 LOGGING = {
     'version': 1,
@@ -21,10 +11,6 @@ LOGGING = {
     'formatters': {
         'default': {
             'format': DEFAULT_LOG_FORMAT,
-        },
-        'json': {
-            'format': DEFAULT_LOG_FORMAT,
-            '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
         },
     },
     'handlers': {
